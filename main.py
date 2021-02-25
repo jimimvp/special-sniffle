e30b5304b3a8b493f1bbfdd4a7a4835aa14bb2d4
Marin Vlastelica <marin.vlastelica@tue.mpg.de>
2021-02-25 21:07:41 +0100

    cem optimization works

--- main.py
+++ main.py
@@ -2,10 +2,10 @@ from src.funcs import *
 from src.utils import load_data
 from src.simulator import Simulator
 from src.simulator import World
+from src.opt.cem import cem_improved
 from collections import namedtuple
-import os
-import sys
 import numpy as np
+import jax
 
 
 file = "./datasets/a.txt"
@@ -14,15 +14,12 @@ config = load_data(file)
 world = World(config=config)
 world.build()
 
-solution_dim = sum([len(intersection.in_streets) for _, intersection in world.intersections.items()])
-traffic_lights = np.random.randint(0, 10, solution_dim)
+d = sum([len(intersection.in_streets) for _, intersection in world.intersections.items()])
+traffic_lights = np.random.randint(0, 10, d)
 i = 0
 for _, intersection in world.intersections.items():
     intersection.traffic_lights = traffic_lights[i:i+len(intersection.in_streets)]
     i+=len(intersection.in_streets)
-
-
-
 
 simulator = Simulator(config=config,
                       world=world)
@@ -31,6 +28,17 @@ for street in world.streets.values():
     street.step()
 
 simulator.start_loop(verbose=True)
+
+
+# run CEM optimization
+
+seed = np.random.randint(0, 1000000)
+key = jax.random.PRNGKey(seed)
+
+key, _ = jax.random.split(key)
+
+d = sum([len(intersection.in_streets) for _, intersection in world.intersections.items()])
+mu, std, sol = cem_improved(key, config, 100, d, 7, num_elites=10, max_keep_elites=10)
 
 
 
---
+++ src/opt/cem.py
@@ -0,0 +1,102 @@
+from operator import gt
+import os
+import numpy as np
+from jax import numpy as jnp
+import jax
+    
+
+
+def score_fn(finishing_times, config):
+     
+    score = np.zeros(len(finishing_times))
+    for i, ft in enumerate(finishing_times):
+        ft = np.array(ft)
+        score[i] = config.car_score*len(ft)  + np.sum(config.simulation_duration - ft)
+    
+    return score
+
+def sample_trajectories(traffic_lights, config):
+
+    # TODO parallelize this
+    res = []
+    for tf in traffic_lights:
+    
+        # ramdom results
+        res.append(np.random.randint(1, 10, np.random.randint(1, 10)))
+    return res
+
+
+
+
+
+
+
+
+def cem_improved(key, config, p, d, K, num_elites=10, mu = None, 
+    std=None, sampling_f=lambda x:x, sampling_d=None, max_keep_elites=None, score_fn=score_fn, 
+    act_low=None,act_high=None):
+    
+    if sampling_d is None:
+        sampling_d = d
+
+    elites = None
+
+    # step 0: define initial distribution
+    if act_low is None:
+        act_low, act_high = -1, 1
+    if mu is None:
+        mu = jnp.ones((sampling_d))*(act_high+act_low)/2
+    if std is None:
+        std = jnp.ones((sampling_d))*1
+
+
+    for _ in range(K):
+
+        # step 1: sample initial trajectories from initial distribution
+        keys = jax.random.split(key, 2)
+
+        # laplacian sample
+        actions = jax.random.laplace(keys[-1], (p,sampling_d))*std + mu
+    
+        actions = jnp.clip(actions, act_low, act_high)
+
+        keys = jax.random.split(keys[-1])
+
+        # transform actions in case of sampling from lower dimension
+        actions_exec = sampling_f(actions)
+
+        finishing_times = sample_trajectories(actions_exec, config)
+        finishing_times = np.array(finishing_times)
+
+        # check if previous elites contain perticularly good elites
+        if max_keep_elites and not elites is None:
+
+            l_prev = min(max_keep_elites, len(elites))
+            prev_elites = elites[:l_prev]
+
+            #print(prev_elites.shape, prev_elite_returns.shape, prev_elite_states.shape, prev_elite_next_obs.shape, prev_elite_costs.shape)
+            actions = jnp.vstack([actions, prev_elites])
+            finishing_times = np.hstack([finishing_times, prev_finishing_times])
+
+
+
+        scores = score_fn(finishing_times, config)
+        # step 2: calculate returns
+
+        # step 3: get elites
+        elite_idxs = (-scores).argsort()[:num_elites]
+
+        print("Best elite:", scores[elite_idxs[0]])
+        print("Worst elite:", scores[elite_idxs[-1]])
+  
+
+        elites = actions[elite_idxs]
+        prev_finishing_times = finishing_times[elite_idxs]
+
+        # step 4: refit initial distribution
+        mu = jnp.mean(elites, axis=0)
+        std = jnp.std(elites, axis=0)
+
+    return mu, std, sampling_f(elites[0][0])
+
+
